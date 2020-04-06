3676cc8d8087b1bc28e94a5ebc58cc21d0b23fac
Fernando Martins <fernando150199@outlook.com>
2020-04-06 18:13:01 -0300

    CRUD ATVIDADES E DISCIPLINAS

---
+++ Controllers/AtividadesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackEnd.Models;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtividadesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public AtividadesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Atividades
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Atividade>>> GetAtividade()
+        {
+            return await _context.Atividade.ToListAsync();
+        }
+
+        // GET: api/Atividades/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Atividade>> GetAtividade(int id)
+        {
+            var atividade = await _context.Atividade.FindAsync(id);
+
+            if (atividade == null)
+            {
+                return NotFound();
+            }
+
+            return atividade;
+        }
+
+        // PUT: api/Atividades/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAtividade(int id, Atividade atividade)
+        {
+            if (id != atividade.IdAtividade)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(atividade).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AtividadeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Atividades
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Atividade>> PostAtividade(Atividade atividade)
+        {
+            _context.Atividade.Add(atividade);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAtividade", new { id = atividade.IdAtividade }, atividade);
+        }
+
+        // DELETE: api/Atividades/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Atividade>> DeleteAtividade(int id)
+        {
+            var atividade = await _context.Atividade.FindAsync(id);
+            if (atividade == null)
+            {
+                return NotFound();
+            }
+
+            _context.Atividade.Remove(atividade);
+            await _context.SaveChangesAsync();
+
+            return atividade;
+        }
+
+        private bool AtividadeExists(int id)
+        {
+            return _context.Atividade.Any(e => e.IdAtividade == id);
+        }
+    }
+}
---
+++ Controllers/DisciplinasController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackEnd.Models;
+using BackEnd.Filters;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisciplinasController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public DisciplinasController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Disciplinas
+        [HttpGet("/api/Disciplina")]
+        [Authorize]
+        [TokenEmailFilter]
+        public async Task<ActionResult<IEnumerable<Disciplina>>> GetDisciplina()
+        {
+            return await _context.Disciplina.ToListAsync();
+        }
+
+        // GET: api/Disciplinas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Disciplina>> GetDisciplina(int id)
+        {
+            var disciplina = await _context.Disciplina.FindAsync(id);
+
+            if (disciplina == null)
+            {   
+                return NotFound();
+            }
+
+            return disciplina;
+        }
+
+        // PUT: api/Disciplinas/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Professor,Adm")]
+        [TokenEmailFilter]
+        public async Task<IActionResult> PutDisciplina(int id, Disciplina disciplina)
+        {
+            if (id != disciplina.IdDisciplina)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(disciplina).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DisciplinaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Disciplinas
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        [Authorize(Roles = "Professor,Adm")]
+        [TokenEmailFilter]
+        public async Task<ActionResult<Disciplina>> PostDisciplina(Disciplina disciplina)
+        {
+            _context.Disciplina.Add(disciplina);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDisciplina", new { id = disciplina.IdDisciplina }, disciplina);
+        }
+
+        // DELETE: api/Disciplinas/5
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Professor,Adm")]
+        [TokenEmailFilter]
+        public async Task<ActionResult<Disciplina>> DeleteDisciplina(int id)
+        {
+            var disciplina = await _context.Disciplina.FindAsync(id);
+            if (disciplina == null)
+            {
+                return NotFound();
+            }
+
+            _context.Disciplina.Remove(disciplina);
+            await _context.SaveChangesAsync();
+
+            return disciplina;
+        }
+
+        private bool DisciplinaExists(int id)
+        {
+            return _context.Disciplina.Any(e => e.IdDisciplina == id);
+        }
+    }
+}
